b83edac14caa6115e4c3404fdbee2237b325636c
Sam Callahan <callahan.l.sam@gmail.com>
2020-01-06 10:03:24 -0600

    Fixed decimal place error, added scipy.stats rework

--- stats.py
+++ stats.py
@@ -1,4 +1,6 @@
 import scipy.stats as stats
+import itertools as it
+import pandas as pd
 
 # What probability distribution would you use to model the scenario outlined above?
 # Binomial
@@ -11,8 +13,10 @@ n_list = [10, 20]
 def get_probs(n, p):
     inv = 1 - p
     results = {}
-    results['half'] = 1 - (inv ** (n / 2))
-    results['all_but_one'] = inv * (p ** (n - 1))
+    results['half or more'] = 0
+    for i in range((n // 2) + 1):
+        results['half or more'] += 1 - (inv  ** i)
+    results['all but one'] = (p ** (n - 1))
     results['all'] = p ** n
     return results
 
@@ -20,3 +24,16 @@ probs = {}
 for n in n_list:
     for p in p_list:
         probs[f'{n}, {p}'] = get_probs(n, p)
+
+# stats rework
+
+def get_stats(n, p):
+    return {
+        'n': n,
+        'p': p,
+        'half or more': stats.binom(n, p).sf(n / 2),
+        'all but one': stats.binom(n, p).pmf(n - 1),
+        'all': stats.binom(n, p).pmf(n)
+    }
+
+stats = pd.DataFrame([get_stats(n, p) for n, p in it.product(n_list, p_list)])
